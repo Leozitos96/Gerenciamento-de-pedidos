d13538972663e2aae376201e1d3e97d2a8ea3e1b
MrShaclone <59567502+MrShaclone@users.noreply.github.com>
2025-02-26 00:34:36 -0300

    Menu
    Adicionado o menu

--- GerenciamentoDePedidos/GerenciamentoDePedidos/Program.cs
+++ GerenciamentoDePedidos/GerenciamentoDePedidos/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace GerenciamentoDePedidos
 {
@@ -6,7 +7,84 @@ namespace GerenciamentoDePedidos
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            while (true)
+            {
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("=====================================");
+                Console.WriteLine("    *** Menu De Gerenciamento ***       ");
+                Console.WriteLine("=====================================");
+                Console.WriteLine();
+                Console.ResetColor();
+                Console.WriteLine(" 1 - Cadastrar Cliente");
+                Console.WriteLine(" 2 - Cadastrar Produto");
+                Console.WriteLine(" 3 - Criar Pedido");
+                Console.WriteLine(" 4 - Listar Pedidos");
+                Console.WriteLine(" 5 - Deletar Cliente");
+                Console.WriteLine(" 6 - Deletar Produto");
+                Console.WriteLine(" 7 - Sair");
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(" Escolha uma opção (1-7): ");
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("=====================================");
+                Console.ResetColor();
+
+                //Resumindo para vcs, o ForegroundColor vai mudar a cor dos writeline, e o resetColor é obvio né, volta ao normal
+
+                Console.ForegroundColor = ConsoleColor.DarkBlue;
+                string opc = Console.ReadLine();
+
+                switch (opc)
+                {
+                    case "7":
+                        Console.WriteLine("\nEncerrando o programa...");
+                        Thread.Sleep(2500);
+                        return;
+                        break;
+
+                    case "1":
+
+                        Thread.Sleep(2500);
+                        return;
+                        break;
+
+                    case "2":
+
+                        Thread.Sleep(2500);
+                        return;
+                        break;
+
+                    case "3":
+
+                        Thread.Sleep(2500);
+                        return;
+                        break;
+
+                    case "4":
+
+                        Thread.Sleep(2500);
+                        return;
+                        break;
+
+                    case "5":
+
+                        Thread.Sleep(2500);
+                        return;
+                        break;
+
+                    case "6":
+
+                        Thread.Sleep(2500);
+                        return;
+                        break;
+
+                    default:
+                        Console.WriteLine("Opção invalida!");
+                        Thread.Sleep(1500);
+                        break;
+                }
+            }
         }
     }
 }
